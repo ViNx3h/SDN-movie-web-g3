cc50eca2ff3024ca7e9ecec3ecdf1cdbd774e2e0
NguyenThanhDat <nguyenthanhdat09072003@gmail.com>
2025-03-03 20:32:32 +0700

    ThanhDat

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-
-import axios from 'axios';
+import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -14,26 +13,31 @@ import Explore from "./Pages/Explore.tsx";
 import List from "./Pages/List.tsx";
 import SearchPage from "./Pages/SearchPage.tsx";
 import { store } from "./store/store.tsx";
+import Booking from "./Pages/Booking.tsx";
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
-axios.defaults.headers.common['Authorization'] = `Bearer ${import.meta.env.VITE_APP_ACCESS_TOKEN}`
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+axios.defaults.headers.common["Authorization"] = `Bearer ${
+  import.meta.env.VITE_APP_ACCESS_TOKEN
+}`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <div className=" pb-14 lg:pb-0">
         <BrowserRouter>
-
           <Header />
 
           <Routes>
-
             <Route path="/" element={<App />}></Route>
             <Route path="/list/:id" element={<List />}></Route>
             <Route path=":detail" element={<Explore />}></Route>
             <Route path=":detail/:id" element={<Detail />}></Route>
             <Route path="/search" element={<SearchPage />}></Route>
+            <Route
+              path="/booking/:detail/:id"
+              element={<Booking />}
+            ></Route>{" "}
+            {/* Thêm tuyến đường Booking */}
           </Routes>
           <Footer />
           <MobileNavigation />
