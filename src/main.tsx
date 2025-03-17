be21390ec63f07fca6cbc4bcd550045d790446a7
NguyenThanhDat <nguyenthanhdat09072003@gmail.com>
2025-03-17 08:46:18 +0700

    sssffdd

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import axios from "axios";
-=======
-import axios from 'axios';
->>>>>>> 58c1a6a7b35b47e8e6e85957fc1678738c3b1820
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -18,21 +14,20 @@ import List from "./Pages/List.tsx";
 import SignUp from "./Pages/SignUp.tsx";
 // import SearchPage from "./Pages/SearchPage.tsx";
 
-import { store } from "./store/store.tsx";
 import Booking from "./Pages/Booking.tsx";
+import SearchPage from "./Pages/SearchPage.tsx";
+import SignIn from "./Pages/SignIn.jsx";
+import { store } from "./store/store.tsx";
 
-<<<<<<< HEAD
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 axios.defaults.headers.common["Authorization"] = `Bearer ${
   import.meta.env.VITE_APP_ACCESS_TOKEN
 }`;
-=======
-import SignIn from './Pages/SignIn.jsx';
 
-
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
-axios.defaults.headers.common['Authorization'] = `Bearer ${import.meta.env.VITE_APP_ACCESS_TOKEN}`
->>>>>>> 58c1a6a7b35b47e8e6e85957fc1678738c3b1820
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+axios.defaults.headers.common["Authorization"] = `Bearer ${
+  import.meta.env.VITE_APP_ACCESS_TOKEN
+}`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -45,20 +40,15 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             <Route path="/list/:id" element={<List />}></Route>
             <Route path=":detail" element={<Explore />}></Route>
             <Route path=":detail/:id" element={<Detail />}></Route>
-<<<<<<< HEAD
             <Route path="/search" element={<SearchPage />}></Route>
             <Route
               path="/booking/:detail/:id"
               element={<Booking />}
             ></Route>{" "}
             {/* Thêm tuyến đường Booking */}
-=======
             {/* <Route path="/search" element={<SearchPage />}></Route> */}
-            
             <Route path="/signin" element={<SignIn />}></Route>
             <Route path="/signup" element={<SignUp />}></Route>
-
->>>>>>> 58c1a6a7b35b47e8e6e85957fc1678738c3b1820
           </Routes>
           <Footer />
           <MobileNavigation />
